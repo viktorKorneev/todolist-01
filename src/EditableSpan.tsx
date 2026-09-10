f080608cb6200f53ce7e87a088d01e35de95815a
viktorKorneev <korneevviktor90@yandex.ru>
2026-09-10 19:19:31 +0300

    step-28

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,21 +1,30 @@
-import {useState} from "react";
+import {ChangeEvent, useState} from "react";
 
 type Props = {
     value: string
 }
 
 export const EditableSpan = ({value}: Props) => {
+    const [title, setTitle] = useState(value)
     const [isEditMode, setIsEditMode] = useState(false)
+
+    const changeTitle = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
 
     const turnOnEditMode = () => {
         setIsEditMode(true)
+    }
+
+    const turnOffEditMode = () => {
+        setIsEditMode(false)
     }
 
 
     return (
         <>
             {isEditMode ? (
-                <input value={value} autoFocus/>
+                <input value={title} onChange={changeTitle} onBlur={turnOffEditMode} autoFocus />
             ) : (
                 <span onDoubleClick={turnOnEditMode}>{value}</span>
             )}
