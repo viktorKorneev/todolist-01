abb8c1aa699747283cdc8800bdecd31bf6dcf82d
viktorKorneev <korneevviktor90@yandex.ru>
2026-09-18 14:11:27 +0300

    step-31

--- src/CreateItemForm.tsx
+++ src/CreateItemForm.tsx
@@ -1,5 +1,6 @@
 import {ChangeEvent, KeyboardEvent, useState} from "react";
-import {Button} from "./Button.tsx";
+import {Button} from "@mui/material";
+
 
 type Props = {
     onCreateItem: (title: string) => void,
@@ -37,7 +38,7 @@ export const CreateItemForm = ({onCreateItem}: Props) => {
                    value={taskTitle}
                    onChange={changeItemTitleHandler}
                    onKeyDown={createItemOnEnterHandler}/>
-            <Button title={"+"} onClick={createItemHandler}/>
+            <Button variant="contained" onClick={createItemHandler}>+</Button>
             {error && <div className={"error-message"}>{error}</div>}
 
             {/*/!*<input ref={inputRef}/>*!/---------------------*/}
