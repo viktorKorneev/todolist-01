7e35772cddb2479bef46e1dc9eb06a63f1886db8
viktorKorneev <korneevviktor90@yandex.ru>
2026-09-25 08:41:44 +0300

    step-42

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -4,7 +4,13 @@ import {Todolist} from "../App.tsx";
 const initialState: Todolist[] = []
 
 export const todolistsReducer = (state: Todolist[] = initialState, action: Actions): Todolist[] => {
-
+    switch (action.type) {
+        case "delete_todolist" : {
+            return state
+        }
+        default:
+            return state
+    }
 }
 
 const action = {
